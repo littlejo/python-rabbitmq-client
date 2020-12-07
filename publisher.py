f67d1cd4dd675c96677cf3849232f8bd988b09a9
root <root@ip-10-0-64-124.eu-west-3.compute.internal>
2020-12-07 11:08:13 +0000

    first commit

---
+++ publisher.py
@@ -0,0 +1,15 @@
+import pika, logging
+from settings import url
+logging.basicConfig()
+
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+
+connection = pika.BlockingConnection(params) # Connect to CloudAMQP
+channel = connection.channel() # start a channel
+channel.queue_declare(queue='pdfprocess') # Declare a queue
+# send a message
+
+channel.basic_publish(exchange='', routing_key='pdfprocess', body='User information')
+print ("[x] Message sent to consumer")
+connection.close()
